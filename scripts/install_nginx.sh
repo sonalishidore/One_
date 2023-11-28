cb2b09085f9dd9d68c23ed54bb5010c91618daff
sonalishidore <147718713+sonalishidore@users.noreply.github.com>
2023-11-28 23:12:42 +0530

    Update install_nginx.sh

--- scripts/install_nginx.sh
+++ scripts/install_nginx.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 sudo yum update
-sudo amazon-linux-extras install nginx1
+#sudo amazon-linux-extras install nginx1
+sudo amazon-linux-extras install nginx1.12
+nginx -v
+systemctl start nginx
+systemctl enable nginx
+chmod 2775 /usr/share/nginx/html 
+find /usr/share/nginx/html -type d -exec chmod 2775 {} \;
+find /usr/share/nginx/html -type f -exec chmod 0664 {} \;
 
