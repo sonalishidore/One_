75a80adb71c2ddfc2d58c6385a22789e8d969cd5
sonalishidore <147718713+sonalishidore@users.noreply.github.com>
2023-11-29 08:26:51 +0530

    Update install_nginx.sh

--- scripts/install_nginx.sh
+++ scripts/install_nginx.sh
@@ -1,14 +1,5 @@
  #!/bin/bash
  sudo yum update -
  sudo update install nginx1 -y
- sudo systemctl start nginx && sudo systemctl enable nginx
- sudo systemctl status nginx
- nano /etc/nginx/sites-available/default
- server {
-        listen 80 default_server;
-        #listen [::]:80 default_server;
-        }
-  sudo ln -s /etc/nginx/sites-available/websitename /etc/nginx/sites-enabled
-  nginx -t
-  sudo systemctl restart nginx
+ 
  
